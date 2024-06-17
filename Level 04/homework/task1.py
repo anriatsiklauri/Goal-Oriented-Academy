391db85582a564871ff328b545318353ff45346a
andria <andriatsiklauri.2015gmail.com>
2024-06-17 14:16:24 +0400

    commit

---
+++ Level 04/homework/task1.py
@@ -0,0 +1,14 @@
+#2)  შექმენი ცვლადები,  სახელი,გვარი,ასაკი,ჰობი,ქვეყანა და  ეს ყველა ცვლადი დაპრინტეთ.
+#ტოლობის მარცხნივ იწერება სახელი მაგალითად name და surname
+#ტოლობა არის მნიშვნელობის მინიჭება
+#ტოლობის მარჯვნივ  იწერება ის არის მნიშვნელობა
+name = "andria"
+surname = "tsiklauri"
+age = 9
+my_hobby = "simgera da dakvra"
+country = "georgia"
+print(name)
+print(surname)
+print(age)
+print(my_hobby)
+print(country)
---
+++ Level 04/homework/task2.py
@@ -0,0 +1,10 @@
+#3) შექმენით  ცვლადები,  სახელი,გვარი,ასაკი,ჰობი,ქვეყანა და  ეს ყველა ცვლადი დაუმატეთ ერთმანეთს.
+# # ძიეზით არის დაკომენტარება
+
+name = "andria"
+surname = "tsiklauri"
+age = "9"
+my_hobby = "simgera da dakvra"
+country = "georgia"
+print(name + " " + surname + " " + age + " " + my_hobby + " " + country) #ბრჭყალები გვჭირდება 
+
